2a3e90939bb0debc19cf0a93a5dfa09fbcfd8f6b
joel.rcampos296@gmail.com <joel.rcampos296@gmail.com>
2024-06-10 09:55:02 -0500

    Initial commit

---
+++ js/work.js
@@ -0,0 +1,21 @@
+var work = [
+    {
+        title: "First Project",
+        pic: "/img/coding.jpeg"
+    },
+
+    {
+        title: "Second Project",
+        pic: "http://www.animal-photography.com/thumbs/red_tabby_long_hair_kitten_~AP-0UJFTC-TH.jpg"
+    },
+
+    {
+        title: "Third Project",
+        pic: "http://www.animal-photography.com/thumbs/silver_tabby_kittens~AP-0JO6Y9-TH.jpg"
+    },
+
+    {
+        title: "Fourth Project",
+        pic: "http://www.animal-photography.com/thumbs/silver_tabby_kitten_looking_up~AP-0DLVMB-TH.jpg"
+    }
+];
